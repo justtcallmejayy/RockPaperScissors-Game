b72bb4221f525962dfba0bd868b20e9e5d9b2f2a
JAY CHOKSI <jaychokxii1804@gmail.com>
2024-11-15 15:23:42 -0500

    Added some logic of Script, styled the buttons, and changed index format

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Code for creating logic for the Game
+
+let userScore =0;
+let computerScore =0;
+
+const choices = document.querySelectorAll('.choice');
+
+choices.forEach(choice => {
+    console.log(choice);
+    // Add event listener to each choice to handle user's choice
+    choice.addEventListener('click', function() {
+        console.log("Choices was clicked");
+    });
+    
+});
